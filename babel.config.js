67a4a728eb31f03e45603e7955f635475a495c0a
Megamia <luudat214@gmaill.com>
2024-03-25 09:17:26 +0700

    update

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  variants: {
-    extend: {
-      opacity: ['disabled'],
-      cursor: ['disabled'],
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  ]
 }
